e903c07a429977ffce263b4520162c84ecd22c64
PReichetanz <paul.reichetanz@gmx.de>
2022-04-23 11:06:44 +0200

    Add random functionality

--- src/hooks/useJokes.ts
+++ src/hooks/useJokes.ts
@@ -3,6 +3,14 @@ import { Joke } from '../lib/types';
 import useLocalStorage from './useLocalStorage';
 
 export default function useJokes() {
+  const [usedJokesId, setUsedJokesId] = useLocalStorage<string[]>(
+    'usedJokesId',
+    []
+  );
+  const [jokeIdsToBeUsed, setJokeIdsToBeUsed] = useLocalStorage<string[]>(
+    'jokeIdsToBeUsed',
+    jokes.map(joke => joke.id) // set jokes.ids here as initial value?
+  );
   const [jokeOfTheDay, setJokeOfTheDay] = useLocalStorage<Joke>(
     'jokeOfTheDay',
     {
@@ -13,8 +21,34 @@ export default function useJokes() {
   );
 
   function getRandomJoke() {
-    const randomIndex = getRandomIndex(0, jokes.length - 1);
-    setJokeOfTheDay(jokes[randomIndex]);
+    const randomIndex = getRandomIndex(0, jokeIdsToBeUsed.length - 1);
+    let selectedJoke: Joke;
+    if (usedJokesId.length === 0) {
+      selectedJoke = jokes[randomIndex];
+      filterUsedJokes(selectedJoke);
+      return;
+    }
+    if (jokeIdsToBeUsed.length === 0) {
+      selectedJoke = jokes[randomIndex];
+      restartFilter(selectedJoke);
+      return;
+    } else {
+      const unusedJokes = jokes.filter(joke => !usedJokesId.includes(joke.id));
+      selectedJoke = unusedJokes[randomIndex];
+    }
+    filterUsedJokes(selectedJoke);
+  }
+
+  function filterUsedJokes(joke: Joke) {
+    setUsedJokesId([...usedJokesId, joke.id]);
+    setJokeIdsToBeUsed(jokeIdsToBeUsed.filter(id => id !== joke.id));
+    setJokeOfTheDay(joke);
+  }
+
+  function restartFilter(joke: Joke) {
+    setUsedJokesId([joke.id]);
+    setJokeIdsToBeUsed(jokes.map(joke => joke.id).filter(id => id !== joke.id));
+    setJokeOfTheDay(joke);
   }
 
   function getRandomIndex(min: number, max: number) {
