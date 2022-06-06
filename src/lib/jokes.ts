bfb7366a96f9f9a00417b265b819398cb17c3c5c
PReichetanz <paul.reichetanz@gmx.de>
2022-06-06 21:58:51 +0200

    Refactor with new components

--- src/lib/jokes.ts
+++ src/lib/jokes.ts
@@ -9,91 +9,91 @@ const jokes = [
     question: 'Auf dem Todesstern ist heute Vadertag.',
     answer: '',
   },
-  {
-    id: 3,
-    question: 'Wohin bringen Bananen vormittags ihre kleinen Kinder?',
-    answer: 'In die Chiquita.',
-  },
-  {
-    id: 4,
-    question: 'Hab mir ein Dach gekauft.',
-    answer: "Ging auf's Haus.",
-  },
-  {
-    id: 5,
-    question: 'Wie nennt man einen Waschbären, der viel trainiert?',
-    answer: 'Waschmaschine',
-  },
-  {
-    id: 6,
-    question: 'Was sagt ein Pirat, wenn er trockenes Gras sieht?',
-    answer: 'A, Heu.',
-  },
-  {
-    id: 7,
-    question: 'Der Bademeister geht in Rente.',
-    answer: 'Er hatte eine chlorreiche Karriere.',
-  },
-  {
-    id: 8,
-    question: 'Ich bin heute aus dem Töpferkurs geflogen.',
-    answer: 'Hab mich wohl im Ton vergriffen.',
-  },
-  {
-    id: 9,
-    question: '"Warum biegen Sie die Äste dieser Tanne gerade?"',
-    answer: '"Ich bin Kieferorthopäde."',
-  },
-  {
-    id: 10,
-    question: '"Ich stecke in der Bredouille!"',
-    answer: '"Oh wie schön, Frankreich!"',
-  },
-  {
-    id: 11,
-    question: 'Wer Nachtisch sagt, mousse au chocolat sagen.',
-    answer: '',
-  },
-  {
-    id: 12,
-    question: 'Bei Hühnern heißt das Chicksal, oder?',
-    answer: '',
-  },
-  {
-    id: 13,
-    question: '"Mensch, Dein Hund bellt aber tief!"',
-    answer: '"Ja, ist ein Subwuffer."',
-  },
-  {
-    id: 14,
-    question: 'Habe gestern zwei Biber beim Essen beobachtet.',
-    answer: 'Es gab Steg.',
-  },
-  {
-    id: 15,
-    question: 'Captain Kirk hat immer Brückentag.',
-    answer: '',
-  },
-  {
-    id: 16,
-    question: 'Welcher Vogel hat Darth Vader auf den Kopf gekackt?',
-    answer: 'Der Star Wars.',
-  },
-  {
-    id: 17,
-    question: '"Warum ist deine Wohnung so dreckig?"',
-    answer: '"I don\'t care."',
-  },
-  {
-    id: 18,
-    question: 'Für wen kauft ein Egoist Obst?',
-    answer: 'Pfirsich!',
-  },
-  {
-    id: 19,
-    question: 'Liberal als gar kein Fisch.',
-    answer: '(wait for it)',
-  },
+  // {
+  //   id: 3,
+  //   question: 'Wohin bringen Bananen vormittags ihre kleinen Kinder?',
+  //   answer: 'In die Chiquita.',
+  // },
+  // {
+  //   id: 4,
+  //   question: 'Hab mir ein Dach gekauft.',
+  //   answer: "Ging auf's Haus.",
+  // },
+  // {
+  //   id: 5,
+  //   question: 'Wie nennt man einen Waschbären, der viel trainiert?',
+  //   answer: 'Waschmaschine',
+  // },
+  // {
+  //   id: 6,
+  //   question: 'Was sagt ein Pirat, wenn er trockenes Gras sieht?',
+  //   answer: 'A, Heu.',
+  // },
+  // {
+  //   id: 7,
+  //   question: 'Der Bademeister geht in Rente.',
+  //   answer: 'Er hatte eine chlorreiche Karriere.',
+  // },
+  // {
+  //   id: 8,
+  //   question: 'Ich bin heute aus dem Töpferkurs geflogen.',
+  //   answer: 'Hab mich wohl im Ton vergriffen.',
+  // },
+  // {
+  //   id: 9,
+  //   question: '"Warum biegen Sie die Äste dieser Tanne gerade?"',
+  //   answer: '"Ich bin Kieferorthopäde."',
+  // },
+  // {
+  //   id: 10,
+  //   question: '"Ich stecke in der Bredouille!"',
+  //   answer: '"Oh wie schön, Frankreich!"',
+  // },
+  // {
+  //   id: 11,
+  //   question: 'Wer Nachtisch sagt, mousse au chocolat sagen.',
+  //   answer: '',
+  // },
+  // {
+  //   id: 12,
+  //   question: 'Bei Hühnern heißt das Chicksal, oder?',
+  //   answer: '',
+  // },
+  // {
+  //   id: 13,
+  //   question: '"Mensch, Dein Hund bellt aber tief!"',
+  //   answer: '"Ja, ist ein Subwuffer."',
+  // },
+  // {
+  //   id: 14,
+  //   question: 'Habe gestern zwei Biber beim Essen beobachtet.',
+  //   answer: 'Es gab Steg.',
+  // },
+  // {
+  //   id: 15,
+  //   question: 'Captain Kirk hat immer Brückentag.',
+  //   answer: '',
+  // },
+  // {
+  //   id: 16,
+  //   question: 'Welcher Vogel hat Darth Vader auf den Kopf gekackt?',
+  //   answer: 'Der Star Wars.',
+  // },
+  // {
+  //   id: 17,
+  //   question: '"Warum ist deine Wohnung so dreckig?"',
+  //   answer: '"I don\'t care."',
+  // },
+  // {
+  //   id: 18,
+  //   question: 'Für wen kauft ein Egoist Obst?',
+  //   answer: 'Pfirsich!',
+  // },
+  // {
+  //   id: 19,
+  //   question: 'Liberal als gar kein Fisch.',
+  //   answer: '(wait for it)',
+  // },
 ];
 
 export default jokes;
